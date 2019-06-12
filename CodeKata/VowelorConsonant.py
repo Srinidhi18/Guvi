6798335519d93d9cb7ad2ddc8a45ab16ec77d09c
Srinidhi18 <51684986+Srinidhi18@users.noreply.github.com>
2019-06-12 14:10:28 +0530

    Update VowelorConsonant.py

--- CodeKata/VowelorConsonant.py
+++ CodeKata/VowelorConsonant.py
@@ -1,9 +1,16 @@
 x=input()
 c = ['a','e','i','o','u','A','E','I','O','U']
-if (x is c):
-  print("Vowel")
-elif (x not in c):
-  if((x>'A' and x<'Z') or (x>'a' and x<'z')):
-    print("Consonant")
+if x.isalpha():
+  if (x in c):
+     print("Vowel")
   else:
+   print("Consonant")
+else: 
     print("invalid")
+
+
+
+
+ 
+
+ 
