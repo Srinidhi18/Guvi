2a05ff46eb55e520625e3e7309128e5f7285331f
Srinidhi18 <51684986+Srinidhi18@users.noreply.github.com>
2019-06-13 12:14:33 +0530

    Create Armstrong or not.py

---
+++ CodeKata/Armstrong or not.py
@@ -0,0 +1,14 @@
+def arm():
+  sum = 0
+  t = a
+  while t > 0:
+    c = t % 10
+    sum += c ** 3
+    t //= 10          
+  if a == sum:
+    print('yes')
+  else: 
+    print('no')
+a=int(input()) 
+if(a<=100000):
+  arm()  
