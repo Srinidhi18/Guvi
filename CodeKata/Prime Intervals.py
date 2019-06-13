91b8a4de94e65bc8f2ba3b8c83287c886db7bfde
Srinidhi18 <51684986+Srinidhi18@users.noreply.github.com>
2019-06-13 15:09:13 +0530

    Create Prime Intervals.py

---
+++ CodeKata/Prime Intervals.py
@@ -0,0 +1,10 @@
+x,y = map(int,input().split())   
+  
+for i in range(x+1,y):  
+   if i > 1:  
+       for k in range(2,i):  
+           if (i % k) == 0:  
+               break  
+       else:  
+           print(i,end=' ')  
+            
